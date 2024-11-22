4ea42ef07dc797606a8882e46ec5f6993daebe8a
LB-Squishy <laurentleboulbar@gmail.com>
2024-11-22 14:14:04 +0100

    fix: :sparkles: retour GET pour partage des recherches

--- app/Views/templates/nosLivres.php
+++ app/Views/templates/nosLivres.php
@@ -7,9 +7,9 @@
 <section class="nosLivres-section">
     <div class="nosLivres-section__header">
         <h1><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></h1>
-        <form method="POST" action="/nos-livres" class="champs-recherche">
+        <form method="GET" action="/nos-livres" class="champs-recherche">
             <button type="submit" class="champs-recherche__button"><i class="fa-solid fa-magnifying-glass"></i></button>
-            <input type="text" name="rechercheLivre" placeholder="Rechercher un livre" class="champs-recherche__input" value="<?= isset($_POST["rechercheLivre"]) ? htmlspecialchars($_POST["rechercheLivre"], ENT_QUOTES, 'UTF-8') : "" ?>">
+            <input type="text" name="rechercheLivre" placeholder="Rechercher un livre" class="champs-recherche__input" value="<?= isset($_GET["rechercheLivre"]) ? htmlspecialchars($_GET["rechercheLivre"], ENT_QUOTES, 'UTF-8') : "" ?>">
         </form>
     </div>
 
