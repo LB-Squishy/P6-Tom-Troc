15c891dcf565654409204a592b5344db32aadc18
LB-Squishy <laurentleboulbar@gmail.com>
2024-11-22 13:59:39 +0100

    feat: :zap: REQUETE EN post

--- app/Controllers/NosLivresController.php
+++ app/Controllers/NosLivresController.php
@@ -16,8 +16,8 @@ class NosLivresController extends AbstractController
         $data = [];
         $bookManager = new BookManager();
 
-        if (isset($_GET["rechercheLivre"]) && !empty(trim($_GET["rechercheLivre"]))) {
-            $nomlivre = htmlspecialchars(trim($_GET["rechercheLivre"]), ENT_QUOTES, 'UTF-8');
+        if (isset($_POST["rechercheLivre"]) && !empty(trim($_POST["rechercheLivre"]))) {
+            $nomlivre = htmlspecialchars(trim($_POST["rechercheLivre"]), ENT_QUOTES, 'UTF-8');
             $data['books'] = $bookManager->getBookByTitle(16, $nomlivre);
         } else {
             $data['books'] = $bookManager->getLastBook(16);
