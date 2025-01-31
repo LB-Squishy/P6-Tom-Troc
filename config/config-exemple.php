850a24a2f28e857ab90268fe8ace4a1e46d57567
LB-Squishy <laurentleboulbar@gmail.com>
2025-01-31 14:26:51 +0100

    feat: :zap: gitignore

---
+++ config/config-exemple.php
@@ -0,0 +1,14 @@
+<?php
+// Les constantes du layout
+define('TEMPLATE_VIEW_PATH', './app/Views/templates/');
+define('LAYOUT_VIEW_PATH', './app/Views/layout.php');
+
+// Le chemin des images
+define('PROFILE_IMAGE_PATH', '/src/images/uploads/profile/');
+define('BOOK_SMALL_COVER_PATH', '/src/images/uploads/book/');
+
+// les données de connexions à la bdd
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tom_troc');
+define('DB_USER', 'votre username');
+define('DB_PASS', 'votre mdp');
